ef641cdf3d1f79c1f8c4683508beab475083003c
Jeremi Sz <JeremiSzlapka@gmail.com>
2023-11-09 20:14:51 +0000

    added disk saving

--- src/server/wishlists.go
+++ src/server/wishlists.go
@@ -2,8 +2,13 @@ package server
 
 import (
 	"log"
+	"os"
 	"strings"
 	"sync"
+)
+
+const (
+	fILE = "saved.txt"
 )
 
 type Wishlist struct {
@@ -23,29 +28,61 @@ var (
 )
 
 func (w *Wishlist) SaveWishlist(id uint8, value string) {
-	w.lock.Lock()
-	defer w.lock.Unlock()
 	if id > 7 {
 		return
 	}
+	w.lock.Lock()
+	defer w.lock.Unlock()
+	w.loadFromFile()
 	w.wishlist[id] = value
 	log.Println("Saved wishlist for id: ", id, "to", value)
 
 }
 
 func (w *Wishlist) LoadWishlists(id uint8) Data {
-	w.lock.RLock()
-	defer w.lock.RUnlock()
 	if id > 7 {
 		return Data{}
 	}
+	w.lock.RLock()
+	defer w.lock.RUnlock()
+
 	targetId := findTarget(id)
 
 	list := w.wishlist[targetId]
 	items := strings.Split(list, "\n")
+	w.saveToFile()
 	return Data{w.wishlist[id], items, names[targetId], id}
 }
 
 func findTarget(id uint8) uint8 {
 	return mapping[id]
+}
+
+func (w *Wishlist) saveToFile() {
+	f, err := os.OpenFile(fILE, os.O_WRONLY, 0777)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer f.Close()
+	for a := range w.wishlist {
+		f.WriteString(w.wishlist[a])
+		f.WriteString(":")
+	}
+	f.Sync()
+}
+func (w *Wishlist) loadFromFile() {
+	f, err := os.OpenFile(fILE, os.O_RDONLY, 0777)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer f.Close()
+	b := make([]byte, 1024)
+	f.Read(b)
+	s := string(b)
+	values := strings.Split(s, ":")
+	for i := 0; i < 8; i++ {
+		w.wishlist[i] = values[i]
+	}
 }
